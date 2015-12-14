1cd8534dfd57527cf16ff7fe33fec9d41aafe2fd
Paul Austian Alday <paul.austian.alday@gmail.com>
2015-12-14 23:55:16 +0800

    fuck you

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -4,34 +4,11 @@
 class Home extends Controller{
 
 	public function index(){
-		$model = $this->model('Game');
-		$this->render('home/index',["items" => $model->all_games()]);
+		if(isset($_SESSION['id'])){
+			$name = $_SESSION['username'];
+		}else{
+			$name = 'you';
+		}
+		$this->render('home/index',['name'=>$name]);
 	}
-
-	public function show($id){
-		$model = $this->model('Game');
-		$this->render('home/view',["items" => $model->view_game($id)]);
-	}
-
-	public function refresh(){
-		$model = $this->model('Game');
-		print_r(json_encode($model->all_games()));
-	}
-
-	public function history($id){
-		$game = $this->model('Game');
-		$score = $this->model('Score');
-		$this->render('scores/show',['items'=>$score->show($id),'game'=>$game->view_game($id)]);
-	}
-
-	public function refreshshow($id){
-		$model = $this->model('Game');
-		print_r(json_encode($model->view_game($id)));
-	}
-
-	public function ended(){
-		$model = $this->model('Game');
-		$this->render('home/index',["items" => $model->all_ended_games()]);
-	}
-
 }
